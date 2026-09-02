95ef9ca03321b1df0f3f87d5490ece262c5b3a79
Kone Gbah Ulrich Paul David <ulrichkone124@gmail.com>
2026-09-02 14:44:18 +0000

    - Remplacement de la valeur statique par mutableStateOf("") - Utilisation de remember et du délégué 'by' pour conserver la saisie lors des recompositions - Connexion du rappel onValueChange pour mettre à jour amountInput

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -20,6 +20,10 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -76,9 +80,10 @@ fun TipTimeLayout() {
 
 @Composable
 fun EditNumberField(modifier: Modifier = Modifier) {
+    var amountInput by remember { mutableStateOf("") }
     TextField(
-        value = "",
-        onValueChange = {},
+        value = amountInput,
+        onValueChange = { amountInput = it },
         modifier = modifier
     )
 }
