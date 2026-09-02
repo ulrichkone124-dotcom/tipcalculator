45b7c8cb1302324274aa07f54ff9c775fd28e712
Kone Gbah Ulrich Paul David <ulrichkone124@gmail.com>
2026-09-02 15:54:00 +0000

    - Utilisation de toDoubleOrNull() avec l'opérateur Elvis (?: 0.0) pour gérer les valeurs nulles - Calcul du pourboire via la fonction calculateTip(amount)

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -83,7 +83,9 @@ fun TipTimeLayout() {
 @Composable
 fun EditNumberField(modifier: Modifier = Modifier) {
     var amountInput by remember { mutableStateOf("") }
+
     val amount = amountInput.toDoubleOrNull() ?: 0.0
+    val tip = calculateTip(amount)
 
     TextField(
         value = amountInput,
